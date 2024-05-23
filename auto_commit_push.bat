bf4b1b164c468aeac01b190fc2ac378bc4c7eaad
Shruti Sharma <shruts1103@gmail.com>
2024-05-23 13:17:25 +0530

    Added new button

--- auto_commit_push.bat
+++ auto_commit_push.bat
@@ -1,18 +1,36 @@
+@REM @echo off
+@REM :: Check for changes
+@REM git status --porcelain > changes.txt
+@REM if %errorlevel% neq 0 goto nochanges
+
+@REM :: Stage all changes
+@REM git add .
+
+@REM :: Commit changes with a timestamp
+@REM set "datetime=%date% %time%"
+@REM git commit -m "Auto-commit on %datetime%"
+
+@REM :: Push changes to the remote repository
+@REM git push
+
+@REM :nochanges
+@REM echo No changes to commit
+@REM del changes.txt
+
 @echo off
-:: Check for changes
-git status --porcelain > changes.txt
-if %errorlevel% neq 0 goto nochanges
+set FILE_PATH=C:\React 2\React_workk
+set COMMIT_MESSAGE=Automated update
 
-:: Stage all changes
-git add .
+echo Updating file...
+echo Hello, world! > %FILE_PATH%
 
-:: Commit changes with a timestamp
-set "datetime=%date% %time%"
-git commit -m "Auto-commit on %datetime%"
+echo Staging changes...
+git add %FILE_PATH%
 
-:: Push changes to the remote repository
+echo Committing changes...
+git commit -m "%COMMIT_MESSAGE%"
+
+echo Pushing changes to GitHub...
 git push
 
-:nochanges
-echo No changes to commit
-del changes.txt
+echo Done.
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Commit Made</div>;
+  return <div>Helllo Shruti here</div>;
 };
 
 export default Page;
