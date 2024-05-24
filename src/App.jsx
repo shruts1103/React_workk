13b3d499d5385bffe53a9ffb680b4343ca854600
Shruti Sharma <shruts1103@gmail.com>
2024-05-24 12:52:34 +0530

    icon change size

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,94 @@
+import {
+  Camera,
+  HolidayVillage,
+  Logout,
+  Person,
+  Settings,
+  VideoCall,
+} from "@mui/icons-material";
 import "./App.css";
 import { Button } from "@mui/material";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+
 function App() {
   return (
     <>
-      <div>
-        <Button>Hello Shruti</Button>
+      <div className="container">
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<Person></Person>}
+        >
+          Friends
+        </Button>
+        <br></br>
+        <br></br>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<HolidayVillage></HolidayVillage>}
+        >
+          Homepage
+        </Button>
+        <br></br>
+        <br></br>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<FormatListBulletedIcon></FormatListBulletedIcon>}
+        >
+          Lists
+        </Button>
+        <br></br>
+        <br></br>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<Camera></Camera>}
+        >
+          Camera
+        </Button>
+        <br></br>
+        <br></br>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<VideoCall></VideoCall>}
+        >
+          Videos
+        </Button>
+        <br></br>
+        <br></br>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<Settings></Settings>}
+        >
+          Settings
+        </Button>
+        <br></br>
+        <br></br>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          sx={{ width: "120px", height: "40px" }}
+          startIcon={<Logout></Logout>}
+        >
+          Logout
+        </Button>
       </div>
     </>
   );
--- src/components/Page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>Helllo Shruti here</div>;
-};
-
-export default Page;
